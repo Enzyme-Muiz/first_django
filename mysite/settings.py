0345bc1126e51da62584d288ed6783629278c114
Muizdeen Raji <muizdeenraji@gmail.com>
2021-01-13 22:06:27 +0000

    s3-dfs

--- mysite/settings.py
+++ mysite/settings.py
@@ -164,4 +164,4 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 AWS_S3_REGION_NAME = 'eu-west-2'
 
-DEFAULT_FILE_STORAGES= "storages.backends.s3boto3.S3boto3Storage" 
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage" 
